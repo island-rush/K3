c9160f196f6c82d0352837b119af4a5632ca63e2
spenceradolph <spenceradolph@ymail.com>
2020-01-06 18:07:39 -0600

    added better comment

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ app.use('/', router);
 // Statically serve all frontend files
 app.use(express.static(`${__dirname}/react-client/build`)); // TODO: Use middleware or reverse proxy to serve static files -> aka, anything with res.sendFile()
 
-// Handle 404
+// Handle 404 (if no other route is hit in router or static serve)
 app.use((req: Request, res: Response) => {
     res.status(404).sendFile(`${__dirname}/server/pages/404.html`);
 });
