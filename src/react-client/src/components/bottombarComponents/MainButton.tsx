e16bb0b8ad962d927f7f77d4794fdf9cb210bd44
Anderson <1501068760@mil>
2020-01-10 14:24:42 -0700

    confirm dialog box on main button click

--- src/react-client/src/components/bottombarComponents/MainButton.tsx
+++ src/react-client/src/components/bottombarComponents/MainButton.tsx
@@ -47,13 +47,15 @@ export class MainButton extends Component<Props> {
                 style={mainButtonStyle}
                 onClick={event => {
                     // normally confirms are obtrusive UI, and should use something else TODO: confirm dialog box...
-                    // eslint-disable-next-line no-restricted-globals
-                    // if (confirm("Are you sure you want to move on?")) {
-                    // 	this.props.mainButtonClick();
-                    // }
-                    event.preventDefault();
-                    mainButtonClick();
-                    event.stopPropagation();
+                    //eslint-disable-next-line no-restricted-globals
+                    if(gameStatus != WAITING_STATUS){
+                        if (window.confirm("Are you sure you want to move on?")) {
+                            this.props.mainButtonClick();
+                        }
+                        event.preventDefault();
+                        mainButtonClick();
+                        event.stopPropagation();
+                    }
                 }}
             >
                 {buttonText}
