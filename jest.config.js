f8d568cfc08f8f4008eb464e3392c1e1487b49a8
spenceradolph <spenceradolph@ymail.com>
2020-01-09 19:30:59 -0600

    stubbing tests for shops, figuring out mocks

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    }
+};
