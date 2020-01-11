fc57d517743151848f2b434096e54336fe72534e
spenceradolph <spenceradolph@ymail.com>
2020-01-11 17:14:54 -0600

    updates to testing

--- src/tests/purchasing.test.ts
+++ src/tests/purchasing.test.ts
@@ -1,17 +1,41 @@
-import md5 from 'md5';
-import { PURCHASE_PHASE_ID } from '../constants';
-import { Game } from '../server/classes';
+// import md5 from 'md5';
+// import { BLUE_TEAM_ID, PURCHASE_PHASE_ID, TYPE_MAIN, SERVER_SHOP_PURCHASE_REQUEST, TANK_COMPANY_TYPE_ID } from '../constants';
+// import { shopPurchaseRequest } from '../server/actions';
+// import { Game } from '../server/classes';
+// import { ShopPurchaseRequestAction } from '../types';
 
-const testGameId = 999;
+// const testGameId = 999;
 
-beforeAll(async () => {
-    const testGame = await Game.add('z1z1', 'test', md5('test'), { gameId: testGameId });
-    testGame.setPhase(PURCHASE_PHASE_ID);
-});
+// beforeAll(async () => {
+//     const testGame = await Game.add('z1z1', 'test', md5('test'), { gameId: testGameId });
+//     testGame.setPhase(PURCHASE_PHASE_ID);
+// });
 
-afterAll(async () => {
-    const testGame = new Game(testGameId);
-    await testGame.delete();
-});
+// afterAll(async () => {
+//     const testGame = new Game(testGameId);
+//     await testGame.delete();
+// });
 
-describe('purchasing', () => {});
+// jest.mock('../server/helpers');
+
+// describe('purchasing', async () => {
+//     const session: any = {
+//         ir3: {
+//             gameId: testGameId,
+//             gameTeam: BLUE_TEAM_ID,
+//             gameControllers: [TYPE_MAIN]
+//         },
+//         socketId: 'test'
+//     };
+
+//     const action: ShopPurchaseRequestAction = {
+//         type: SERVER_SHOP_PURCHASE_REQUEST,
+//         payload: {
+//             shopItemTypeId: TANK_COMPANY_TYPE_ID
+//         }
+//     };
+
+//     const results = await shopPurchaseRequest(session, action);
+
+//     test('something', async () => {});
+// });
