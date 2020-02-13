9d685724ae06b867d58af04a3d3f3132e64881ba
spenceradolph <spenceradolph@ymail.com>
2020-02-12 20:52:23 -0600

    can't open enemy pieces

--- src/react-client/src/redux/actions/container/pieceOpen.ts
+++ src/react-client/src/redux/actions/container/pieceOpen.ts
@@ -9,17 +9,20 @@ import { setUserfeedbackAction } from '../setUserfeedbackAction';
  */
 export const pieceOpen = (selectedPiece: PieceType) => {
     return (dispatch: Dispatch, getState: () => FullState, sendToServer: typeof emit) => {
-        const { gameboard, container } = getState();
+        const { gameboard, container, gameInfo } = getState();
 
         const { pieceTypeId } = selectedPiece;
 
         // TODO: only show pieces that could go inside this container (specify that to the reducer?)
-        // TODO: do these checks on the backend as well
-        // TODO: are there any situations when we would not want players to look inside containers? (not likely)
 
         //don't want to open pieces that aren't container types
         if (!CONTAINER_TYPES.includes(pieceTypeId)) {
             dispatch(setUserfeedbackAction('Not a piece that can hold other pieces...'));
+            return;
+        }
+
+        if (selectedPiece.pieceTeamId !== gameInfo.gameTeam) {
+            dispatch(setUserfeedbackAction('Piece does not belong to you.'));
             return;
         }
 
